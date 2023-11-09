081ce42e09bdc33bfd73331eb593ab875a594257
Juan Jose <chevike15@gmail.com>
2023-11-09 00:14:59 -0600

    Pagina de tablas de multiplicar

--- src/pages/Tablas.jsx
+++ src/pages/Tablas.jsx
@@ -5,13 +5,54 @@ export default function Tablas() {
         Tablas de Multiplicar
       </h1>
 
-      <article className="text-center">
-        <img
-          src="/src/assets/tablas.jpg"
-          alt="tablas de multiplicar"
-          className="img-fluid w-25"
-        />
-      </article>
+      <p>
+        Para aprender a multiplicar debes de practicar mucho las tablas, desde
+        la tabla del 1 hasta la tabla del 10, una vez se te quedan estas 10
+        tablas, todo se te hace más fácil y los problemas matemáticos son más
+        rápidos de resolver.
+      </p>
+
+      <section className="mb-3">
+        <article>
+          <h2 className="text-success">
+            ¿Por qué es tan importante aprender las tablas de multiplicar?
+          </h2>
+          <p>
+            Porque las tablas de multiplicar son necesarias para los demás
+            grados y también en el ciclo básico, porque ahí se incluyen en
+            cálculos más complicados.
+          </p>
+        </article>
+
+        <article className="text-center">
+          <img
+            src="/src/assets/tablas-multiplicar-1-6.jpg"
+            alt="tablas de multiplicar"
+            className="img-fluid w-75"
+          />
+        </article>
+      </section>
+
+      <section className="mb-3">
+        <article>
+          <h2 className="text-success">
+            ¿Cómo puedo aprender las tablas de multiplicar?
+          </h2>
+          <p>
+            Para aprender las tablas de multiplicar debes de practicar mucho,
+            puedes hacerlo con la ayuda de tus padres, hermanos o amigos, pero
+            también puedes hacerlo solo.
+          </p>
+        </article>
+
+        <article className="text-center">
+          <img
+            src="/src/assets/tablas-multiplicar-7-12.jpg"
+            alt="tablas de multiplicar"
+            className="img-fluid w-75"
+          />
+        </article>
+      </section>
     </>
   );
 }
