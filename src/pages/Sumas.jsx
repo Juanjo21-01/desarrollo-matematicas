567de3b5d755b6044c233ebcac2346dba2fbed81
Juan Jose <chevike15@gmail.com>
2023-11-09 00:28:43 -0600

    Pagina de suma de quebrados

--- src/pages/Sumas.jsx
+++ src/pages/Sumas.jsx
@@ -3,13 +3,57 @@ export default function Sumas() {
     <>
       <h1 className="text-primary fs-1 text-center py-4">Suma de Quebrados</h1>
 
-      <article className="text-center">
-        <img
-          src="/src/assets/sumas.webp"
-          alt="suma de quebrados"
-          className="img-fluid w-25"
-        />
-      </article>
+      <p>
+        La suma de quebrados o fracciones es una operación que se realiza cuando
+        se tienen dos o más fracciones y se quiere saber el resultado de la suma
+        de las fracciones. Pero antes de realizar la suma de quebrados, debemos
+        de tener en cuenta que las fracciones deben de tener el mismo
+        denominador. Y si no tienen el mismo denominador, debemos de buscar el
+        mínimo común múltiplo (mcm) de los denominadores de las fracciones.
+      </p>
+
+      <section className="mb-3">
+        <article>
+          <h2 className="text-success">
+            Suma de fracciones con el mismo denominador
+          </h2>
+          <p>
+            Para sumar fracciones con el mismo denominador se tienen que sumar
+            los numeradores dejando el mismo denominador.
+          </p>
+        </article>
+
+        <article className="text-center">
+          <img
+            src="/src/assets/suma-mismo-denominador.jpg"
+            alt="suma de quebrados con mismo denominador"
+            className="img-fluid w-50"
+          />
+        </article>
+      </section>
+
+      <section className="mb-3">
+        <article>
+          <h2 className="text-success">
+            Suma de fracciones con distinto denominador
+          </h2>
+          <p>
+            Para hacer suma de fracciones con distinto denominador, lo primero
+            que hay que hacer es poner un denominador común: esto es el mínimo
+            común múltiplo entre los denominadores que haya. Después
+            multiplicamos cada numerador por el número que hayamos multiplicado
+            al denominador.
+          </p>
+        </article>
+
+        <article className="text-center">
+          <img
+            src="/src/assets/suma-diferente-denominador.jpg"
+            alt="suma de quebrados con distinto denominador"
+            className="img-fluid w-50"
+          />
+        </article>
+      </section>
     </>
   );
 }
